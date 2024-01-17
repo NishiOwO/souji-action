5714fc6fcd7f5e19153774c8cd45214265fc9aa5
Haruka <33943897+hulk510@users.noreply.github.com>
2024-01-18 02:36:50 +0900

    feat: clean up github action caches (#5)
    Release-As: 1.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+
+const deleteRefActionsCache = async (
+  octokit: ReturnType<typeof github.getOctokit>,
+  repo: { owner: string; repo: string },
+  ref: string
+): Promise<void> => {
+  // Get the list of cache IDs
+  // https://github.com/octokit/plugin-paginate-rest.js#octokitpaginate
+  const iterator = octokit.paginate.iterator(
+    octokit.rest.actions.getActionsCacheList,
+    {
+      ...repo,
+      ref
+    }
+  )
+
+  // https://github.com/octokit/octokit.js/tree/b831b6bce43d56b97e25a996e1b43525486d8bd3?tab=readme-ov-file#pagination
+  for await (const { data: cacheList } of iterator) {
+    for (const { id: cacheId } of cacheList) {
+      if (!cacheId) continue
+      await octokit.rest.actions.deleteActionsCacheById({
+        ...repo,
+        cache_id: cacheId
+      })
+    }
+  }
+}
 
 /**
  * The main function for the action.
@@ -7,18 +34,36 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const token = core.getInput('repo-token')
+    const octokit = github.getOctokit(token)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // get repostiory information
+    const { repo } = github.context
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // MEMO: payloadから取得できるのは確認したけど、型何もついてない
+    const payload = github.context.payload
+    const prNumber = payload.pull_request?.number
+    const headRef = payload.pull_request?.head?.ref as string | undefined
+    const ref = payload.ref as string | undefined
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    if (prNumber) {
+      // fire when event is pull_request or pull_request_target or pull_request_review or pull_request_review_comment
+      core.info(`delete cache for refs/pull/${prNumber}/merge`)
+      await deleteRefActionsCache(octokit, repo, `refs/pull/${prNumber}/merge`)
+      core.info('done ✅')
+    }
+    if (headRef) {
+      // fire when event is pull_request or pull_request_target or pull_request_review or pull_request_review_comment
+      core.info(`delete cache for refs/heads/${headRef}`)
+      await deleteRefActionsCache(octokit, repo, `refs/heads/${headRef}`)
+      core.info('done ✅')
+    }
+    if (ref) {
+      // fire when event is workflow_dispatch or push
+      core.info(`delete cache for ${ref}`)
+      await deleteRefActionsCache(octokit, repo, ref)
+      core.info('done ✅')
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
